eac4d71adde18c032e57e54c1a37e515371bcee1
Jackson Sá <jacksonwendel@gmail.com>
2025-02-09 00:03:27 -0300

    feat: adiciona ícone do plugin
    - Converte logo.svg para PNG 128x128
    - Adiciona referência ao ícone no cabeçalho do plugin

--- AmigoPetWp/amigopet-wp.php
+++ AmigoPetWp/amigopet-wp.php
@@ -12,6 +12,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       amigopet-wp
  * Domain Path:       /languages
+ * Plugin Icon:       assets/images/icon-128x128.png
  */
 
 // If this file is called directly, abort.
